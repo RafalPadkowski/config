4b8b189a6bd097d84f7e5cc8f1dea7e146088de0
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-10-10 08:10:57 +0200

    feat: add settings

---
+++ src/config/settings.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class Setting:
+    label: str
+    current_value: Any
+
+
+@dataclass
+class SettingOption:
+    display_str: str
+    value: Any
+
+
+@dataclass
+class SettingOptions(Setting):
+    options: list[SettingOption]
+
+
+@dataclass
+class SettingBoolean(Setting):
+    current_value: bool
